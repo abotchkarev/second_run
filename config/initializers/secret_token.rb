9b97d53510b3b941fa5221dbfec3dcbb1a80b463
abotchkarev <alexei.botchkarev@gmail.com>
2011-06-25 23:44:18 -0400

    Second run just created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondRun::Application.config.secret_token = '5678928fb6da2fc904cd3515c8ba008252d6468f49deb9f990aece3d4f3abac10c3c881a164e26ccdea50a4c301435d0265d98b4a073466fee9d72fb1d3df14c'
