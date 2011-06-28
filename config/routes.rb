e512ab4088866b37999ee24165a6badfdb7de66d
abotchkarev <alexei.botchkarev@gmail.com>
2011-06-28 19:15:59 -0400

    Finished Capter 7: User model with password, and user show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 SecondRun::Application.routes.draw do
-    
-
 
   root :to => 'pages#home'
-  
+
   resources :users
-  
+
   match '/signup',  :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
-
-
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
