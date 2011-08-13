0bfaaf293daa4ef1197d7381bee4c5498fad6fa9
abotchkarev <alexei.botchkarev@gmail.com>
2011-08-12 22:37:02 -0400

    rake updated

--- db/migrate/20110706071746_add_relationship_to_appointments.rb
+++ db/migrate/20110706071746_add_relationship_to_appointments.rb
@@ -1,11 +1,9 @@
 class AddRelationshipToAppointments < ActiveRecord::Migration
   def self.up
-    remove_column :appointments, :user_id
-    remove_column :appointments, :project_id
+
   end
 
   def self.down
-    add_column :appointments, :user_id
-    add_column :appointments, :project_id
+
   end
 end
